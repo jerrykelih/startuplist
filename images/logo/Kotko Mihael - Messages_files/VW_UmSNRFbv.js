5c6e8623446abdf93536afb541b1b9e9c764d862
Jerry Jernej Kelih <jerry@jerrykelih.com>
2014-03-27 15:57:42 +0100

    sds

--- images/logo/Kotko Mihael - Messages_files/VW_UmSNRFbv.js
+++
@@ -1,5 +0,0 @@
-/*!CK:288669856!*//*1384743539,178146625*/
-
-if (self.CavalryLogger) { CavalryLogger.start_js(["XuZmm"]); }
-
-__d("ReactPropTypeLocations",["keyMirror"],function(a,b,c,d,e,f){"use strict";var g=b('keyMirror'),h=g({props:null,context:null,childContext:null});e.exports=h;});
--- images/logo/Kotko Mihael - Messages_files/yClJuv2bauS.js
+++
@@ -1,5 +0,0 @@
-/*!CK:4192013196!*//*1381871947,178191655*/
-
-if (self.CavalryLogger) { CavalryLogger.start_js(["8oJIO"]); }
-
-__d("legacy:bug-js",["BugBar"],function(a,b,c,d){a.BugBar=a.BugBar||b('BugBar');},3);
